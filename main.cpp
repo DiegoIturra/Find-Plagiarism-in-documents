23e8be0b642c23765f41517e169405bfa4e838ee
Diego Iturra <diturra@dparadig.com>
2022-10-15 02:27:25 -0300

    Se agregan funciones que parsean el texto y eliminan espacios/tabulaciones, pendiente eliminar simbolos sin peso el el texto completo

--- main.cpp
+++ main.cpp
@@ -5,43 +5,82 @@
 
 using namespace std;
 
-void parseText(const string& path){
+struct FileStructure{
+	string filename;
+	string text;
+};
 
-}
+//Retorna el texto parseado (READY)
+string parseText(const string& path){
+	ifstream file;
+	string text;
+	string completeText;
 
-vector<string> getPathFromOriginalCorpus(){
-	const string path = "/Users/diego/Desktop/proyectoTopicos/original_corpus";
-
-	vector<string> listOfPathFiles;
-	for(const auto& entry : filesystem::directory_iterator(path)){
-		listOfPathFiles.push_back(entry.path());
+	file.open(path);
+	if(file.is_open()){
+		while(file.good()){
+			file >> text;
+			completeText += text;
+		}
 	}
 
-	return listOfPathFiles;
+	return completeText;
+}
+
+void writeNewDocument(const FileStructure& fileStructure){
+	const string newPathFolder = "/Users/diego/Desktop/proyectoTopicos/preprocessing_corpus/";
+
+	ofstream outputText(newPathFolder + fileStructure.filename,ofstream::out);
+
+	outputText << fileStructure.text;
+}
+
+
+void saveDocumentsIntoNewFolder(const vector<FileStructure> listOfParsedDocuments){
+	for(FileStructure fileStructure : listOfParsedDocuments){
+		writeNewDocument(fileStructure);
+	}
+}
+
+
+//Parsea los documentos y retorna una lista con <filename,text>
+vector<FileStructure> getSetOfParsedDocuments(const vector<string>& listOfFileNames){
+	const string orignalPathFolder = "/Users/diego/Desktop/proyectoTopicos/original_corpus/";
+
+	vector<FileStructure> listOfParsedDocuments;
+	FileStructure fileStructure;
+	for(string filename : listOfFileNames){
+		fileStructure.filename = filename;
+		fileStructure.text = parseText(orignalPathFolder+filename);
+		listOfParsedDocuments.push_back(fileStructure);
+	}
+	return listOfParsedDocuments;
+}
+
+
+
+//Retorna una lista con los nombres de los ficheros originales (READY)
+vector<string> getFilenamesFromOriginalCorpus(){
+	const string path = "/Users/diego/Desktop/proyectoTopicos/original_corpus";
+
+	vector<string> listOfFileNames;
+	for(const auto& entry : filesystem::directory_iterator(path)){
+		listOfFileNames.push_back(entry.path().filename());
+	}
+
+	return listOfFileNames;
 }
 
 int main(){
 	
-	getPathFromOriginalCorpus();
+	//Obtener lista de nombres de los documentos
+	vector<string> listOfFileNames = getFilenamesFromOriginalCorpus();
+
+	//Obtener textos parseados
+	vector<FileStructure> listOfParsedDocuments = getSetOfParsedDocuments(listOfFileNames);
+
+	//Guardar textos parseados en una nueva carpeta con ficheros preprocesados
+	saveDocumentsIntoNewFolder(listOfParsedDocuments);
 
 	return 0;
 }
-
-	/*ifstream file;
-	string text;
-	int counter = 0;
-	for(const auto& entry : filesystem::directory_iterator(path)){
-
-		if(counter > 0) break;
-		file.open(entry.path());
-		cout << entry.path() << endl;
-
-		if(file.is_open()){
-			while(file.good()){
-				file >> text;
-				cout << text;
-			}
-		}
-
-		counter++;
-	}*/
